50c523236b329f2fb072e4572522450fce0c314e
Itzelll <mmitzel2435@gmail.com>
2023-03-13 11:36:00 -0600

    detalles corregidos

--- music.py
+++ music.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 import codecs
 import plotly.express as px
-import matplotlib.pyplot as plt
 
 st.title('Generos de musica')
 DATA_URL = ('top10s.csv')
@@ -12,9 +10,6 @@ DATA_URL = ('top10s.csv')
 def load_data(nrows):
     doc = codecs.open(DATA_URL, 'rU', 'latin1')
     data = pd.read_csv(doc, nrows=nrows)
-    #lowercase = lambda x: str(x).lower()
-    #data.rename({'start_lat':'lat', 'start_lng': 'lon'}, axis=1, inplace=True)
-    #data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN]) #la fecha lo vuelve string
     return data
 
 data_load_state = st.text('Loading data...')
@@ -28,89 +23,95 @@ sidebar.write("Matricula: S20006761")
 sidebar.write("zs20006761@estudiantes.uv.mx")
 sidebar.markdown("___")
 
-#Dataset
-st.header("Dataset")
+#::::::::::Dataset::::::::::::::::::::
+st.header("Dataset:")
 sidebar.title("Opciones:")
 agree = sidebar.checkbox("Selecciona para mostrar el dataset")
 if agree:
   st.dataframe(data)
 
-#nombre canciones
-st.header("Titulo de canciones")
+sidebar.markdown("_")
+
+#:::::::::::nombre canciones:::::::::::
+st.header("Titulo de canciones:")
 sidebar.write("\n")
 @st.cache
 def load_data_film(names):
     doc = codecs.open(DATA_URL, 'rU', 'latin1')
     data = pd.read_csv(doc)
-    filtered_name = data[data['title'].str.contains(names)]
-
+    filtered_name = data[data['title'].str.contains(names, case=False)]
     return filtered_name
 
-selected_title = sidebar.text_input("Busca nombre de una cancion:")
+selected_title = sidebar.text_input("Buscar por nombre de una cancion:")
 button_name = sidebar.button("Buscar titulo")
 
 if(button_name):
     filtroname = load_data_film(selected_title)
+    count_row = filtroname.shape[0]
+    st.write(f"Total encontrados: {count_row}")
     st.dataframe(filtroname)
 
-#genero musica
-st.header("Filtro por genero")
+#:::::::::genero musica:::::::::::
+st.header("Filtro de canciones por genero:")
 sidebar.write("\n")
 @st.cache
 def load_data_bygenre(g):
     doc = codecs.open(DATA_URL, 'rU', 'latin1')
     data = pd.read_csv(doc)
     filtered_data = data[data['top genre'] == g]
-
     return filtered_data
 
-selected_g = sidebar.selectbox("Busca genero de musica:", data['top genre'].unique())
+selected_g = sidebar.selectbox("Buscar por genero de musica:", data['top genre'].unique())
 btnFilterbyG = sidebar.button('Filtrar')
 
 if (btnFilterbyG):
     filtro = load_data_bygenre(selected_g)
     count_row = filtro.shape[0] 
     st.write(f"Total items : {count_row}")
-
     st.dataframe(filtro)
 
-#histograma 
-st.header("Histograma")
+#::::::::::: Multiselect ::::::::::::
+st.header("Multiselect:")
 sidebar.write("\n")
+year = sidebar.multiselect("Selecciona año(s):",
+                                options=data['year'].unique())
+
+df_selection=data.query("year == @year")
+st.write("Años seleccionados", df_selection)
+sidebar.markdown("__")
+
+#::::::::::: histograma :::::::::::
+sidebar.title("Graficas:")
+st.header("Histograma")
 agree = sidebar.checkbox("Clic para ver histograma")
 if agree:
   fig_genre=px.bar(data,
-                    x="top genre",
+                    x=data['top genre'],
                     y=data.index,
                     orientation="v",
                     title="Cantidad de canciones que hay por genero",
-                    labels=dict(total="Total Generos", genre="Top Generos"),
+                    labels=dict(y="index", x="top genre"),
                     color_discrete_sequence=["#7ECBB4"],
                     template="plotly_white")
-
   st.plotly_chart(fig_genre)
 
-#grafica de barras de 2 columnas
+#::::::::::: grafica de barras :::::::::::
 st.header("Grafica de barras")
-sidebar.write("\n")
 agree = sidebar.checkbox("Clic para ver grafica de barras")
 if agree:
-   #filter_data = data[data['top genre']]
-   #canada_pop=filter_data['canadian_pop']
-   #pop=filter_data['pop']
+   title=data['title']
+   year=data['year']
    fig_barra=px.bar(data,
-                    x=data.get['canadian_pop'],
-                    y=data.get['pop'],
+                    x=year,
+                    y=title,
                     orientation="v",
-                    title="Cantidad de canciones que hay por genero",
-                    labels=dict(canada_pop="Pop de canada", pop="Pop"),
-                    color_discrete_sequence=["#7ECBB4"],
+                    title="Nombre de las canciones y el año en que salieron",
+                    labels=dict(year="Year", title="Title"),
                     template="plotly_white")
    st.plotly_chart(fig_barra)
 
-#grafica scatter
+#::::::::::: grafica scatter :::::::::::
 st.header("Grafica Scatter")
-sidebar.write("\n")
 agree = sidebar.checkbox("Clic para ver grafica scatter")
 if agree:
     year=data['year']
@@ -120,7 +121,9 @@ if agree:
                    x=year,
                    y=dnce,
                    color=artist,
-                   title="Danceabilidad de las canciones de los artistas",
+                   title="¿Que tan bailable son las canciones dependiendo del artista? Nivel (0-100)",
                    labels=dict(year="Year", artist="Artist", dnce="Danceability"),
                    template="plotly_white")
     st.plotly_chart(fig_age)
+
+sidebar.markdown("___")
